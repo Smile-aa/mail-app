dfd642ba66fd0363d958c71c2e6e6668ed9ebf90
smile <3381530030@qq.com>
2023-11-13 21:38:52 +0800

    登录数据共享

--- src/views/layout/Home.vue
+++ src/views/layout/Home.vue
@@ -1,49 +1,30 @@
 <template>
   <div class="home">
-
-         <div class="menu-list">
-           <a-menu  mode="inline" theme="dark" :inline-collapsed="collapsed">
-             <a-menu-item key="1">
-             <a-icon type="pie-chart"></a-icon>
-             <span>1</span>
-           </a-menu-item>
-           </a-menu>
-         </div>
-
-    <div :class="{'main-app': true, 'menu-unfold': collapsed}">
-       <div class="main-header">
-         <a-button type="primary" style="margin-bottom: 16px" @click="toggleCollapsed">
-            <a-icon :type="collapsed ? 'menu-unfold' : 'menu-fold'" />
-      </a-button>
-    <div class="breadcrumb">
-       <a-breadcrumb>
-    <a-breadcrumb-item>首页</a-breadcrumb-item>
-    <a-breadcrumb-item><a href=""> 统计</a></a-breadcrumb-item>
-  </a-breadcrumb>
+    <left-menu />
+    <div :class="{'main-app': true, 'menu-unfold': $store.state.collapsed}">
+      <slider-nav />
+      <router-view></router-view>
     </div>
-    <ul class="user-info">
-      <li>欢迎user
-        <a-icon type="down" />
-      </li>
-      <li>退出</li>
-    </ul>
-    </div>
-  </div>
   </div>
 </template>
 
 <script>
 
+import LeftMenu from './components/menu.vue';
+import SliderNav from './components/sliderNav.vue';
+
 export default {
   data() {
     return {
-      collapsed: false,
+
     };
   },
+  components: {
+    LeftMenu,
+    SliderNav,
+  },
   methods: {
-    toggleCollapsed() {
-      this.collapsed = !this.collapsed;
-    },
+
   },
 };
 </script>
---
+++ src/views/layout/components/menu.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="menu-list">
+        <a-menu
+           mode="inline"
+           theme="dark"
+           :inline-collapsed="$store.state.collapsed">
+          <a-menu-item key="1">
+             <a-icon type="pie-chart"></a-icon>
+             <span>1</span>
+          </a-menu-item>
+          <a-menu-item key="2">
+             <a-icon type="desktop"></a-icon>
+             <span>1</span>
+          </a-menu-item>
+        </a-menu>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
